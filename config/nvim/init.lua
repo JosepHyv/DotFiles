0479222e6260994e19a157d7f30fab6e58a6cdf2
Joseph Hynimoto Aguilar Lopez <haguilar@auronix.com>
2024-07-01 21:09:54 -0600

    updates in nvim

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,39 +1,28 @@
+require('plugins')
 require('monokai').setup {}
+require('Comment').setup()
+local opt = vim.opt
 vim.scriptencoding = 'utf-8'
-vim.opt.encoding = 'utf-8'
-vim.opt.fileencoding = 'utf-8'
-vim.opt.mouse = 'a'
-vim.wo.number = true
-vim.opt.cursorline = false
-vim.opt.autoindent = true
-vim.opt.tabstop = 4
-vim.opt.wrap = true
-vim.opt.expandtab = true
-vim.opt.autowrite = true
-vim.opt.showcmd = true
-vim.opt.smarttab = true
-vim.wo.number = true
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.title = true
-vim.opt.relativenumber = true
-vim.opt.cursorline = false
-vim.opt.autoindent = true
-vim.opt.wrap = true
-vim.opt.shell = 'zsh'
-vim.opt.expandtab = true
-vim.opt.tabstop = 4 
-vim.opt.shiftwidth = 4
-vim.opt.autowrite = true
-vim.opt.showcmd = true
-vim.opt.shell = 'zsh'
-vim.opt.smarttab = true
-vim.opt.breakindent = true
-
-
-return require('packer').startup(function(use)
-        use 'tanvirtin/monokai.nvim'
-
-end)
-
+opt.encoding = 'utf-8'
+opt.fileencoding = 'utf-8'
+opt.mouse = 'a'
+opt.number = true
+opt.autoindent = true
+opt.tabstop = 4
+opt.wrap = true
+opt.expandtab = true
+opt.showcmd = true
+opt.smarttab = true
+opt.ignorecase = true
+opt.smartcase = true
+opt.title = true
+opt.relativenumber = true
+opt.cursorline = false
+opt.wrap = true
+opt.shell = 'zsh'
+opt.expandtab = true
+opt.shiftwidth = 4
+opt.autowrite = true
+opt.breakindent = true
+-- vim.cmd [[colorscheme vim]]
 
